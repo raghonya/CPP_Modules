564a5f7606cd94a455fec6e7a3170fff5b80f105
Your Name <you@example.com>
2023-11-03 12:47:17 +0400

    ex00

---
+++ cpp05/ex00/Bureaucrat.cpp
@@ -0,0 +1,30 @@
+#include "Bureaucrat.hpp"
+
+Bureaucrat::Bureaucrat(): name("John"), grade(1) {}
+
+Bureaucrat::Bureaucrat(const std::string name, int grade): name(name), grade(grade)
+{
+	if (grade > 150 || grade < 1)
+	{
+		
+	}
+}
+
+Bureaucrat::Bureaucrat(const Bureaucrat& other): name(other.name), grade(other.grade) {}
+
+Bureaucrat& Bureaucrat::operator=(const Bureaucrat& other)
+{
+	this->grade = other.grade;
+
+	return (*this);
+}
+
+Bureaucrat::~Bureaucrat() {};
+
+void	Bureaucrat::incrementGrade( void ) { this->grade -= 1; }
+
+void	Bureaucrat::decrementGrade( void ) { this->grade += 1; }
+
+int		Bureaucrat::getGrade( void ) const { return (this->grade); }
+
+const std::string	Bureaucrat::getName( void ) const { return (this->name); }
---
+++ cpp05/ex00/Bureaucrat.hpp
@@ -0,0 +1,25 @@
+#ifndef BUREAUCRAT_HPP
+# define BUREAUCRAT_HPP
+
+#include <iostream>
+
+class Bureaucrat
+{
+
+	private:
+		const std::string	name;
+		int					grade;
+	public:
+		Bureaucrat();
+		Bureaucrat(const Bureaucrat& other);
+		Bureaucrat&	operator=(const Bureaucrat& other);
+		~Bureaucrat();
+	public:
+		Bureaucrat(const std::string name, int grade);
+		void				incrementGrade( void );
+		void				decrementGrade( void );
+		int					getGrade( void ) const;
+		const std::string	getName( void ) const;
+};
+
+#endif
