1dade164d20806fb6cde49a9f16b761e6377144e
Your Name <you@example.com>
2023-11-27 16:46:15 +0400

    cpp 05 ex02  dowsnt work

--- cpp05/ex01/main.cpp
+++ cpp05/ex01/main.cpp
@@ -4,19 +4,7 @@ int	main()
 {
 	try
 	{
-		// Bureaucrat	bro("Gag", 15);
-
-		// std::cout << bro << std::endl;
-		Form	dprocicDusMnaluForm("Hamletik", 15, 25);
-
-		std::cout << dprocicDusMnaluForm << std::endl;
-
-		Bureaucrat boz("a", 2);
-		// dprocicDusMnaluForm.beSigned(boz);
-		boz.signForm(dprocicDusMnaluForm);
-		std::cout << dprocicDusMnaluForm.getSign() << std::endl;;
-		boz.signForm(dprocicDusMnaluForm);
-		// dprocicDusMnaluForm.beSigned(boz);
+		Bureaucrat barev("yani", 4);
 		
 	}
 	catch (const std::exception& e)
