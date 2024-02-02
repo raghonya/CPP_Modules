7d4ab2777f1366ab15db689f62f4e4e120ca518c
Your Name <you@example.com>
2024-02-02 21:04:53 +0400

    cpp07

--- cpp07/ex02/Array.hpp
+++ cpp07/ex02/Array.hpp
@@ -8,44 +8,60 @@ class Array
 {
 	private:
 		T				*arr;
-		unsigned int	size;
+		unsigned int	capacity;
 	public:
-		Array()
-		{
-			try { this->arr = new T(); }
-			catch(const std::exception& e)
-			{
-				std::cerr << e.what() << '\n';
-			}
-		}
-		Array(unsigned int n)
-		{
-			try	{ this->arr = new T[n]; }
-			catch(const std::exception& e)
-			{
-				std::cerr << e.what() << '\n';
-			}
-			
-		}
-		Array(const Array& other)
-		{
-			// for (size_t i = 0; i < other.size(); ++i)
+		Array();
+		Array(unsigned int n);
+		Array(const Array& other);
+		Array&	operator=(const Array& other);
+		~Array() { if (this->arr) delete[] arr; }
+		
+		T&	operator[](unsigned int pos) const;
+		unsigned int	size() const;
 
-		}
-		Array&	operator=(const Array& other)
-		{
-			
-		}
-		~Array()
-		{
-
-		}
-		T&	operator[](size_t pos)
-		{
-			if (pos >= this->size)
-				throw std::runtime_error("PRIWET");
-			return (this->arr[pos]);
-		}
 };
+
+template <typename T>
+Array<T>::Array() : arr(NULL), capacity(0) {}
+
+template <typename T>
+Array<T>::Array(unsigned int n)
+{
+	this->arr = new T[n]; 
+	this->capacity = n;
+}
+
+template <typename T>
+Array<T>::Array(const Array& other) : capacity(other.capacity)
+{
+	this->arr = new T[other.capacity];
+	for (unsigned int i = 0; i < this->capacity; ++i)
+		this->arr[i] = other.arr[i];
+}
+
+template <typename T>
+Array<T>&	Array<T>::operator=(const Array& other)
+{
+	if (*this == other)
+		return (*this);
+	if (this->arr)
+		delete[] arr;
+	this->arr = new T[other.capacity];
+	this->capacity = other.capacity;
+	for (unsigned int i = 0; i < this->capacity; ++i)
+		this->arr[i] = other.arr[i];
+	return (*this);
+}
+
+template <typename T>
+T&	Array<T>::operator[](unsigned int pos) const
+{
+	if (pos >= this->capacity)
+		throw std::out_of_range("Out of range");
+	return (this->arr[pos]);
+}
+
+template <typename T>
+unsigned int	Array<T>::size() const { return (this->capacity); }
 
 #endif
